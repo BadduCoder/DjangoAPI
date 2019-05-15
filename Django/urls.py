62618485e85cd82df0077c4c6953dc8e433d137d
Sumit Badsara <sumit7engineer@gmail.com>
2019-05-15 23:45:04 +0530

    Slack subscription Check
    Checking out and working with slack

--- Django/urls.py
+++ Django/urls.py
@@ -1,5 +1,5 @@
 """Django URL Configuration
-
+Just trying out slack
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
